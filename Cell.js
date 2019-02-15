8e73bd6cf0d3e8a3a183fb72252386e41aea1eb6
abdou91140 <46482308+abdou91140@users.noreply.github.com>
2019-02-15 13:56:33 +0100

    Add files via upload

---
+++ Cell.js
@@ -0,0 +1,10 @@
+class Cell {
+    constructor(type, weapon, fighter, lightCell) {
+        this.type = type;
+        this.weapon = weapon;
+        this.fighter = fighter;
+        this.lightCell = lightCell;
+    }
+}
+
+var Cell = new Cell();
---
+++ plateau.js
@@ -0,0 +1,11 @@
+let normalCell = 0;
+let obstacleCell = 1;
+let index = [];
+function randomNumber(min,max) {
+    return Math.floor(Math.random() * min);
+}
+var random = randomNumber();
+
+
+
+
