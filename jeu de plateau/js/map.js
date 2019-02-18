0cc7be7b5931e74b69249f15892555c99a90127a
abdou91140 <46482308+abdou91140@users.noreply.github.com>
2019-02-18 09:01:49 +0100

    Update map.js

--- jeu de plateau/js/map.js
+++ jeu de plateau/js/map.js
@@ -11,56 +11,58 @@ class Map {
              for(let j = 0 ; j<= this.y ; j++) {
                 
                  this.cells[this.x, this.y] = new Cell(normalCells);
+
                      }
             }
         }
     
    placeFighters() {
-       if(randomXY(this.cells).type == normalCell){
-        Ken[randomXY(this.cells)];
-        Ryu[randomXY(this.cells)];
+       console.log();
+       if(this.randomXY(this.cells) !== obstacleCell){
+        Ken[this.randomXY(this.cells)];
+        Ryu[this.randomXY(this.cells)];
     } 
        else if((Ken[this.x] == Ryu[this.x]) || (Ken[this.y] == Ryu[this.y]) || (Ken[this.x] == Ryu[this.x + 1]) || (Ken[this.y] == Ryu[this.y - 1]) || (Ken[this.y] == Ryu[this.y]) || (Ken[this.y] == Ryu[this.y + 1]) || (Ken[this.y] == Ryu[this.y - 1])) {
 
             //Ryu[randomXY(this.cells)];
-            placeFighters()
+            this.placeFighters();
        }
         
     }
 
     placeWeapons(number) {
         for (let i = 0; i < number; i++) {
-            while (randomXY(this.cells).type == normalCell && randomXY(this.cells).type !== fireball && randomXY(this.cells).type !== axe || randomXY(this.cells).type !== sword || randomXY(this.cells).type !== gun || Ken[this.cells] !== this.cells(placeWeapons()) || Ryu[this.cells] !== this.cells(placeWeapons())) {
-                fireball[randomXY(this.cells)];     
-                axe[randomXY(this.cells)];     
-                sword[randomXY(this.cells)];  
-                gun[randomXY(this.cells)];  
+            while (this.randomXY(this.cells) == normalCells && this.randomXY(this.cells).type !== fireball && this.randomXY(this.cells).type !== axe || this.randomXY(this.cells).type !== sword || this.randomXY(this.cells).type !== gun || Ken[this.cells] !== this.cells(placeWeapons()) || Ryu[this.cells] !== this.cells(placeWeapons())) {
+                fireball[this.randomXY(this.cells)];     
+                axe[this.randomXY(this.cells)];     
+                sword[this.randomXY(this.cells)];  
+                gun[this.randomXY(this.cells)];  
             }
            }
        }
     
     placeObstacleCells(number) {
         for (let i = 0; i < number; i++) {
-            randomXY(this.cells).type = obstacleCell;
-         while (randomXY(this.cells).type == obstacleCell) {
+            this.randomXY(this.cells).type = obstacleCell;
+         while (this.randomXY(this.cells).type == obstacleCell) {
          }
-         placeObstacleCells();
+         this.placeObstacleCells();
         }
     }
     //this.lightAccessibleCells(){};
     //this.printOnload(){};
     randomXY() {
-        let randomX = Math.floor(Math.random()* this.cells.length);
-        let randomY = Math.floor(Math.random()* this.cells[this.x].length);
+        console.log()
+        let randomX = Math.floor(Math.random()* this.x);
+        let randomY = Math.floor(Math.random()* this.cells.length);
         return (randomX+randomY);
     }
      
 
 }
 var mapGenerate = new Map(10,10);
-console.log(mapGenerate.generateCells());
-console.log(mapGenerate.placeFighters());
-console.log(mapGenerate.placeObstacleCells());
-console.log(mapGenerate.placeWeapons());
-console.log(mapGenerate.randomXY());
+mapGenerate.this.placeFighters();
+mapGenerate.this.placeObstacleCells();
+mapGenerate.this.placeWeapons();
+mapGenerate.this.randomXY();
 console.log();
