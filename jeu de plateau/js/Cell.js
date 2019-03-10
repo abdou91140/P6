f2b9fef959a33feec30d06bd5e869d8375ef2467
abdou91140 <46482308+abdou91140@users.noreply.github.com>
2019-03-10 18:19:21 +0100

    Update Cell.js

--- jeu de plateau/js/Cell.js
+++ jeu de plateau/js/Cell.js
@@ -1,13 +1,16 @@
 class Cell {
-    constructor(type,weapon, fighter, lightCell,position) {
+    constructor(type,weapon, fighter, lightCell,position, data,imgUrl) {
         this.type = type;
         this.weapon = weapon;
         this.fighter = fighter;
         this.lightCell = lightCell;
         this.position = position;
+        this.data = data;
+        this.imgUrl = imgUrl ;
 
     }
 }
 let cellTypes = {normal : 1,obstacle : 2};
+let cellData = {obstacle :"<data-cell-type = 'cellObstacle'>" , normal : "<data-cell-type = 'cellNormal'>", weapon : "<data-cell-type = 'cellWeapon'>", fighter : "<data-cell-type = 'cellFighter'>" }
 
 
