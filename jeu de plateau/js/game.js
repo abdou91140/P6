21b749e3909a6a9a5baa9fa03fe836a41dcc3993
abdou91140 <46482308+abdou91140@users.noreply.github.com>
2019-03-10 18:18:44 +0100

    Update game.js

--- jeu de plateau/js/game.js
+++ jeu de plateau/js/game.js
@@ -1,10 +1,25 @@
 class game {
-    constructor(mapGame, currentPlayers) {
-        this.mapGame = mapGame;
-        this.currentPlayers = currentPlayers;
+    constructor(mapGame,currentPlayer) {
+       this.mapGame = mapGame;
+        this.currentPlayer = currentPlayer;
         //this.startGame(){};
         //this.gameOver(){};
         //this.nextTurn(){};
         //this.displayInfosOfGame(){};
-    }
+        }
+        move() {
+            $('[src]').click(function () {
+                    mapGenerate.cells.forEach(cellGame => {
+                            cellGame.forEach(cellInGame => {
+                                    if (cellInGame.data === cellData.fighter) {
+                    
+                                        console.log()
+                                    }
+                                    })
+                            })
+                    })
+            }
+        
 }
+var gameGenerate = new game(mapGenerate, fightersArr);
+gameGenerate.move();
