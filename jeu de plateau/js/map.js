35c475f5ea5e4f45ca54b8b6eef392bec453fbf5
abdou91140 <46482308+abdou91140@users.noreply.github.com>
2019-03-10 18:15:42 +0100

    Update map.js

--- jeu de plateau/js/map.js
+++ jeu de plateau/js/map.js
@@ -1,49 +1,62 @@
 class Map {
-    constructor(x, y) {
+    constructor(size) {
         this.cells = [];
-        this.x = x;
-        this.y = y;
+        this.size = size;
     }
     generateCells() {
-
-        for (let x = 0; x < this.x; x++) {
+        for (let x = 0; x < this.size; x++) {
             this.cells[x] = [];
-            for (let y = 0; y < this.y; y++) {
-                this.cells[x][y] = new Cell(cellTypes.normal, null, null, null, (x + "-" + y));
-                //console.log(this.cells)
+            for (let y = 0; y < this.size; y++) {
+                this.cells[x][y] = new Cell(cellTypes.normal, null, null, null,(x + '-' + y),cellData.normal);
+                this.cells.imgUrl ="<img src = '../css/white_square.jpg'>";
+        //console.log(this.cells[x][y])
             }
         }
+        
+        
     }
     placeObstacleCells(numberObstacles) {
         let i = 0;
         while (i < numberObstacles) {
-            let randomXYCells = this.randomXY();
-            let cellObstacle = this.cells[randomXYCells.x][randomXYCells.y];
+            let randomXYCell = this.randomXY();
+            let cellObstacle = this.cells[randomXYCell.x][randomXYCell.y];
+            cellObstacle.imgUrl = "<img src = '../css/red_square.jpg'>";
             if (cellObstacle.type === cellTypes.normal) {
+                if(cellObstacle.data === cellData.normal){
+                    cellObstacle.data = cellData.obstacle
+                }
                 cellObstacle.type = cellTypes.obstacle;
                 i++;
             }
         } //console.log(this.cells)
+        return i;
     }
-    placeFighters() {
-        let randomXYCells = this.randomXY();
-        let cellFighter1 = this.cells[randomXYCells.x][randomXYCells.y];
-        let cellFighter2 = this.cells[randomXYCells.x][randomXYCells.y];
-        if (cellFighter1.type === cellTypes.normal && cellFighter1.fighter === null) {
-            cellFighter1.fighter = Ken;
-            //if(cellFighter.figther.Ken !== cellFighter.figther.Ryu && cellFighter.figther.Ken !== cellFighter.figther.Ryu -1 && cellFighter.figther.Ken !== cellFighter.figther.Ryu+1){ 
-            if (cellFighter2 === cellTypes.normal || cellFighter1 !== cellFighter2 && cellFighter1 !== cellFighter2 + 1 && cellFighter1 !== cellFighter2 - 1 && cellFighter2 !== cellFighter1 + 1 && cellFighter2 !== cellFighter1 - 1) {
-            cellFighter2.fighter = Ryu;
+    placeFighters(fightersArr) {
+       
+        fightersArr.forEach(fighter => {
+            while (true) {
+                let randomXYCell = this.randomXY();
+                let cell = this.cells[randomXYCell.x][randomXYCell.y];
+                if (cell.type === cellTypes.normal && cell.fighter === null &&  cell.fighter !== cell.fighter +1 && cell.fighter !== cell.fighter-1 && cell.fighter !== cell.fighter + 1 && cell.fighter !== cell.fighter - 1) {
+                    if(cell.data === cellData.normal && cell.data !== cellData.obstacle){
+                        cell.data = cellData.fighter;
+                    }
+                    this.cells[randomXYCell.x][randomXYCell.y].fighter = fighter;
+                    break
+                }
             }
-        } // console.log(this.cells)
+        }); // console.log(this.cells)
     }
     placeWeapons(weaponsArr) {
         weaponsArr.forEach(weapon => {
             while (true) {
-                let randomXYCells = this.randomXY();
-                let cell = this.cells[randomXYCells.x][randomXYCells.y];
+                let randomXYCell = this.randomXY();
+                let cell = this.cells[randomXYCell.x][randomXYCell.y];
                 if (cell.type === cellTypes.normal && cell.weapon === null) {
-                    this.cells[randomXYCells.x][randomXYCells.y].weapon = weapon;
+                    if(cell.data === cellData.normal && cell.data !== cellData.obstacle && cell.data !== cellData.fighter){
+                        cell.data = cellData.weapon;
+                    }
+                    this.cells[randomXYCell.x][randomXYCell.y].weapon = weapon;
                     break
                 }
             }
@@ -53,27 +66,72 @@ class Map {
 
 
     randomXY() {
-        let randomX = Math.floor(Math.random() * this.x);
-        let randomY = Math.floor(Math.random() * this.y);
-        return { x: randomX, y: randomY }
+        let randomX = Math.floor(Math.random() * this.size);
+        let randomY = Math.floor(Math.random() * this.size);
+        return {
+            x: randomX,
+            y: randomY
+        }
     }
-    //this.lightAccessibleCells(){};
-    printOnload(){
-        //recupérer les this.cells
-        if(this.cells === cellTypes.normal)
-        $("#map").append(this.cells);
-        //assigner une images 
+    lightAccessibleCells(){
+   this.cells.forEach(imgCell => {
+    $("img").mouseover(function() {
+       $("img").css("width","110")})
+    return imgCell}
+   )
+   
+    };
+    printOnload() {
+        this.cells.forEach(row => {
+            row.forEach(cellInRow => {
+                if (cellInRow.type === cellTypes.normal) {
+                    let normal = this.cells.imgUrl ;
+                    $("#map").append(normal)
+                } 
+                if (cellInRow.type === cellTypes.obstacle) {
+                    let obstacle = cellInRow.imgUrl;
+                    $("#map").append(obstacle)
+                }
+                if (cellInRow.type === cellTypes.normal && cellInRow.weapon === fireball) {
+                    let weapon1 = fireball.imgUrl;
+                    $("#map").append(weapon1)
+                }
+                if (cellInRow.type === cellTypes.normal && cellInRow.weapon === gun) {
+                    let weapon2 = gun.imgUrl;
+                    $("#map").append(weapon2)
+                }
+                if (cellInRow.type === cellTypes.normal && cellInRow.weapon === sword) {
+                    let weapon3 = sword.imgUrl;
+                    $("#map").append(weapon3)
+                }
+                if (cellInRow.type === cellTypes.normal && cellInRow.weapon === axe) {
+                    let weapon4 = axe.imgUrl;
+                    $("#map").append(weapon4)
+                }
+                if (cellInRow.type === cellTypes.normal && cellInRow.fighter === Ken) {
+                    let fighter1 = Ken.imgUrl;
+                    $("#map").append(fighter1)
+                }
+                if (cellInRow.type === cellTypes.normal && cellInRow.fighter === Ryu) {
+                    let fighter2 = Ryu.imgUrl;
+                    $("#map").append(fighter2)
+                }
+                else{
+                    this.printOnload
+                }
+            });
 
+        });
+    }
 
     };
 
-}
-
-var mapGenerate = new Map(10, 10);
+var mapGenerate = new Map(9);
 mapGenerate.generateCells();
-mapGenerate.placeObstacleCells(10);
-mapGenerate.placeFighters();
+mapGenerate.placeObstacleCells(8);
+mapGenerate.placeFighters(fightersArr);
 mapGenerate.placeWeapons(weaponsArr);
 mapGenerate.randomXY();
+mapGenerate.lightAccessibleCells()
 mapGenerate.printOnload();
 
