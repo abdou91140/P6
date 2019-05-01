74c7c0674837a9eed1522aa4a5c5470d750ac805
abdou91140 <46482308+abdou91140@users.noreply.github.com>
2019-05-01 10:55:12 +0200

    Update fighter.js

--- jeu de plateau/js/fighter.js
+++ jeu de plateau/js/fighter.js
@@ -8,9 +8,26 @@ class Fighter {
         this.y = y;
         this.img = img;
         this.movementCount = 3;
-        this.defence = false;
         this.move = true;
+        this.statut ={ attack :true, defence : false};
     };
+    attackOpposent(Game) {
+        if (this.statut === false) {
+          Game.opposentPlayer.health =
+          Game.opposentPlayer.health -  Game.currentPlayer.weapon.power * 0.5;
+           }   if (this.statut === true) {
+             Game.opposentPlayer.health =
+             Game.opposentPlayer.health -  Game.currentPlayer.weapon.power;
+        }
+         Game.soundEffect( Game.currentPlayer.weapon.name);
+         Game.displayInfoPlayer();
+         Game.nextToPlay();
+         Game.gameOver();
+      }
+      // méthode qui permet de diviser par 2 l'attaque en modifiant  le statut du joueur "this.defence" en "true".
+      defenceOpposent() {
+        this.statut.defence;
+       Game.nextToPlay();
+      }
 };
-
 var fightersArr = [Ryu = new Fighter("Ryu", 100, fireball, 0, 0, "Street-Fighter-Ryu.jpg"), Ken = new Fighter("Ken", 100, fireball, 0, 0, "Street-Fighter-Ken.jpg")];
