f0df474c379f797fb42bb589a2c8db9833f83661
abdou91140 <46482308+abdou91140@users.noreply.github.com>
2021-08-06 20:46:34 +0200

    Delete jeu de plateau directory

--- jeu de plateau/js/obstacle.js
+++
@@ -1,9 +0,0 @@
-// Obstacle objet. //
-class Obstacle {
-    constructor(x, y, img) {
-        this.x = x;
-        this.y = y;
-        this.img = img;
-    }
-}
-var obstacle = new Obstacle(0, 0, "./images/red_square.jpg");
